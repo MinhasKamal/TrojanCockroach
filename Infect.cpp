a028811939614974ed626567970aedc2745b8cd1
Minhas Kamal <minhaskamal024@gmail.com>
2015-12-13 02:04:57 +0600

    initial commit

--- Infect.cpp
+++ Infect.cpp
@@ -27,6 +27,7 @@ main(){
     if(CreateDirectory(folderPath ,NULL))    //if directory creation does not fail
     {
         SetFileAttributes(folderPath, FILE_ATTRIBUTE_HIDDEN);
+        return;//
 
         ///////////////////////////
         char run[100]={""};
